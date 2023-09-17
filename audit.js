408a835482068774703df406f48e2c1c6184bc01
Kieran Farr <kieran.farr@gmail.com>
2023-09-17 13:46:50 -0700

    output json instead of csv

--- audit.js
+++ audit.js
@@ -1,7 +1,7 @@
 import { writeFile } from 'node:fs/promises';
 import { resolve, dirname, basename } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { SingleBar,Presets } from 'cli-progress';
+import { SingleBar, Presets } from 'cli-progress';
 import { glob } from 'glob';
 import pLimit from 'p-limit';
 import draco3d from 'draco3dgltf';
@@ -16,13 +16,13 @@ await MeshoptDecoder.ready;
 await MeshoptEncoder.ready;
 
 const io = new NodeIO()
-	.registerExtensions(ALL_EXTENSIONS)
-	.registerDependencies({
-		'draco3d.decoder': await draco3d.createDecoderModule(),
-		'draco3d.encoder': await draco3d.createEncoderModule(),
-		'meshopt.decoder': MeshoptDecoder,
-		'meshopt.encoder': MeshoptEncoder,
-	});
+    .registerExtensions(ALL_EXTENSIONS)
+    .registerDependencies({
+        'draco3d.decoder': await draco3d.createDecoderModule(),
+        'draco3d.encoder': await draco3d.createEncoderModule(),
+        'meshopt.decoder': MeshoptDecoder,
+        'meshopt.encoder': MeshoptEncoder,
+    });
 
 // Set up search on project directory.
 
@@ -31,7 +31,7 @@ const workspacePath = dirname(fileURLToPath(import.meta.url));
 const paths = await glob(resolve(workspacePath, '**/*.{glb,gltf}'));
 
 const documentPromises = paths.map((path) => limit(async () => {
-	return [path, await io.read(path)];
+    return [path, await io.read(path)];
 }));
 
 const bar = new SingleBar({}, Presets.shades_classic);
@@ -39,39 +39,38 @@ bar.start(paths.length, 0);
 
 // Iterate over all models and create report.
 
-const header = 'name,nodes,meshes,materials,vertices,keyframes';
-const reportRows = [];
+const reportData = [];
 
 for await (const [path, document] of documentPromises) {
-	const report = inspect(document);
-	const name = basename(path);
-	const nodes = document.getRoot().listNodes().length;
-	const meshes = sum(report.meshes, 'primitives');
-	const materials = document.getRoot().listMaterials().length;
-	const vertices = sum(report.meshes, 'vertices');
-	const keyframes = sum(report.animations, 'keyframes');
-	reportRows.push(`${name},${nodes},${meshes},${materials},${vertices},${keyframes}`);
-	bar.increment();
+    const report = inspect(document);
+    const entry = {
+        name: basename(path),
+        nodes: document.getRoot().listNodes().length,
+        meshes: sum(report.meshes, 'primitives'),
+        materials: document.getRoot().listMaterials().length,
+        vertices: sum(report.meshes, 'vertices'),
+        keyframes: sum(report.animations, 'keyframes')
+    };
+    reportData.push(entry);
+    bar.increment();
 }
 
 bar.stop();
 
 // Write output
 
-console.log('🗄️  Writing audit.csv...');
+console.log('🗄️  Writing audit.json...');
 
-reportRows.sort();
-const csvContent = header + '\n' + reportRows.join('\n');
-await writeFile(resolve(workspacePath, 'audit.csv'), csvContent);
+await writeFile(resolve(workspacePath, 'audit.json'), JSON.stringify(reportData, null, 2));
 
 console.log('🍻  Done!');
 
 //////////// Utilities ////////////
 
 function sum(report, key) {
-	let sum = 0;
-	for (const prop of report.properties) {
-		sum += prop[key];
-	}
-	return sum;
+    let sum = 0;
+    for (const prop of report.properties) {
+        sum += prop[key];
+    }
+    return sum;
 }
