ab5e65bdbc21d8bf7d07b65ef82026d41adbbaea
Kieran Farr <kieran.farr@gmail.com>
2023-09-17 13:28:58 -0700

    feature: add input and output cli options for pipeline.js
    added docs in readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # citybuilderjr
+
+* source assets from kaykit https://kaylousberg.itch.io/city-builder-bits
+
+asset pipeline utilities
+
+`node audit`
+returns csv of gltf assets in project
+
+`node pipeline`
+takes input directory of assets and optimizes each including adding draco compression to an output directory
+
+screenshot
+for each glb create a screenshot using the screenshot-glb script
--- pipeline.js
+++ pipeline.js
@@ -1,6 +1,6 @@
 import { resolve, dirname, parse } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { SingleBar,Presets } from 'cli-progress';
+import { SingleBar, Presets } from 'cli-progress';
 import { glob } from 'glob';
 import pLimit from 'p-limit';
 import draco3d from 'draco3dgltf';
@@ -8,9 +8,26 @@ import { MeshoptDecoder, MeshoptEncoder } from 'meshoptimizer';
 import { NodeIO, Logger } from '@gltf-transform/core';
 import { ALL_EXTENSIONS } from '@gltf-transform/extensions';
 import { dedup, flatten, dequantize, join, weld, resample, prune, sparse, draco } from '@gltf-transform/functions';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+
+// Parse command-line arguments
+const argv = yargs(hideBin(process.argv))
+    .option('input', {
+        alias: 'i',
+        description: 'Input directory path',
+        type: 'string',
+    })
+    .option('output', {
+        alias: 'o',
+        description: 'Output directory path',
+        type: 'string',
+    })
+    .demandOption(['input', 'output'], 'Please provide both input and output paths')
+    .help()
+    .argv;
 
 // Configure glTF I/O.
-
 await MeshoptDecoder.ready;
 await MeshoptEncoder.ready;
 
@@ -24,18 +41,16 @@ const io = new NodeIO()
 		'meshopt.encoder': MeshoptEncoder,
 	});
 
-// Set up search on project directory.
-
-const limit = pLimit(4); // process up to 4 models at a time.
-const workspacePath = dirname(fileURLToPath(import.meta.url));
-const paths = (await glob(resolve(workspacePath, '**/*.{glb,gltf}')))
+// Set up search on the input directory provided.
+const limit = pLimit(4); 
+const inputDir = resolve(argv.input);  // Use provided input directory
+const paths = (await glob(resolve(inputDir, '**/*.{glb,gltf}')))
 	.filter((path) => !parse(path).name.endsWith('_opt'));
 
 const bar = new SingleBar({}, Presets.shades_classic);
 bar.start(paths.length, 0);
 
 // Iterate over all models and process.
-
 await Promise.all(paths.map((path) => limit(async () => {
 	const document = await io.read(path);
 
@@ -52,7 +67,7 @@ await Promise.all(paths.map((path) => limit(async () => {
 	);
 
 	const name = parse(path).name + '_opt.glb';
-	await io.write(resolve(workspacePath, name), document);
+	await io.write(resolve(argv.output, name), document);  // Use provided output directory
 
 	bar.increment();
 })));
@@ -60,5 +75,3 @@ await Promise.all(paths.map((path) => limit(async () => {
 bar.stop();
 
 console.log('🍻  Done!');
-
-//////////// Utilities ////////////
