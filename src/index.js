57711747553c2e1e81fbbede39ae7c7deb3a881f
Kieran Farr <kieran.farr@gmail.com>
2023-10-12 22:00:11 -0700

    move dependencies to npm packages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 /* global AFRAME, THREE, XMLHttpRequest */
 
+require('aframe-proxy-event-component')
+require('aframe-state-component')
+require('aframe-slice9-component')
+require('aframe-look-at-component')
+require('aframe-atlas-uvs-component')
+require('aframe-proxy-event-component')
 require('./app/grid-cursor.js');
 require('./app/intersection-spawn.js');
 require('./app/snap.js');
