fb8f1095e24b8095d3def2cd8c535f59a10cfb02
Kieran Farr <kieran.farr@gmail.com>
2023-11-19 10:21:51 -0800

    upgrade slice9 component to support newest aframe

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 
 require('aframe-proxy-event-component')
 require('aframe-state-component')
-require('aframe-slice9-component')
 require('aframe-look-at-component')
 require('aframe-atlas-uvs-component')
 require('aframe-proxy-event-component')
 require('aframe-gridhelper-component')
+require('./lib/aframe-slice9-component.js')
 require('./app/grid-cursor.js');
 require('./app/intersection-spawn.js');
 require('./app/snap.js');
